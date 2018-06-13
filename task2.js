38886d15b7d4e1dac919edd53edbc294c6218477
SedaGudieva <33003697+SedaGudieva@users.noreply.github.com>
2018-06-13 14:50:50 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,2 +1,28 @@
 /* Task 2 */
+var a=process.argv[2];
+var b=process.argv[3];
+
+if(b.length >= 10000){
+    return false;
+} 
+
+if(a.length <= 1){
+    return false;
+}  
+if(a.length != b.length){
+    return process.stdout.write( '-1' );
+} 
+
+for(var i = 0; i < a.length; i++){
+    if(a[0] == b[i]){
+        var str =b.substring(i);
+        str = str + b.substring(0, i)
+        if(~a.indexOf(str)){
+            var shift = b.length - i;
+            if (shift > i) shift = i;
+            return process.stdout.write(String(shift) );
+        }else
+        str = "";
+    }
+} process.stdout.write( '-1' );
 /* Put your code here */
